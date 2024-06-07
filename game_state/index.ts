85ea80d016203dca2c0980911c3b0a16e1e29385
glmachado <glmachado@nelogica.com.br>
2024-06-06 23:56:58 -0300

    colors

--- game_state/index.ts
+++ game_state/index.ts
@@ -1,26 +1,43 @@
 import init, { GameNoise } from "./pkg/game_state.js";
 import { GUI } from "dat.gui";
 
+const randSeed = Math.floor(Math.random() * 1000000);
+
+const params = {
+  frequency: 1,
+  frequency2: 1,
+  weight1: 0.9,
+  threshould: 0.5,
+  octaves: 3,
+  octaves2: 3,
+  seed: randSeed,
+  lacunarity: 2.0,
+  persistence: 0.5,
+  resolution: 200,
+  field: "#00ff00",
+  sea: "#0000ff",
+  mountain: "#ffffff",
+  sand: "#ffff00",
+  rgbField: [0, 255, 0],
+  rgbSea: [0, 0, 255],
+  rgbMountain: [255, 255, 255],
+  rgbSand: [255, 255, 0],
+};
+
 init().then(() => {
   const canvas = document.createElement("canvas");
+  canvas.style.width = "800px";
+  canvas.style.height = "800px";
   document.body.appendChild(canvas);
   const ctx = canvas.getContext("2d")!;
-  const SIZE = 800;
-  canvas.width = SIZE;
-  canvas.height = SIZE;
-  const randSeed = Math.floor(Math.random() * 1000000);
+
   const gui = new GUI();
   const noise = GameNoise.new(randSeed);
   //add gui with a callback
-  const params = {
-    frequency: 0.01,
-    threshould: 0.5,
-    octaves: 3,
-    seed: randSeed,
-    lacunarity: 2.0,
-    persistence: 0.5,
-  };
-  const data = new Uint8ClampedArray(SIZE * SIZE * 4);
+
+  noise.set_frequency2(params.frequency2);
+  noise.set_octaves(params.octaves);
+  noise.set_frequency(params.frequency);
 
   gui.add(params, "seed", 0, 1000000).onChange((value) => {
     noise.set_seed(Math.floor(value));
@@ -36,6 +53,38 @@ init().then(() => {
     draw();
   });
 
+  gui.addColor(params, "sea").onChange((value) => {
+    params.rgbSea = hexToRgb(params.sea);
+    draw();
+  });
+  gui.addColor(params, "field").onChange((value) => {
+    params.rgbField = hexToRgb(params.field);
+    draw();
+  });
+  gui.addColor(params, "mountain").onChange((value) => {
+    params.rgbMountain = hexToRgb(params.mountain);
+    draw();
+  });
+
+  gui.addColor(params, "sand").onChange((value) => {
+    params.rgbSand = hexToRgb(params.sand);
+    draw();
+  });
+
+  gui.add(params, "weight1", 0, 1).onChange((value) => {
+    draw();
+  });
+
+  gui.add(params, "frequency2", 1, 1000).onChange((value) => {
+    noise.set_frequency2(value);
+    draw();
+  });
+
+  gui.add(params, "octaves2", 1, 10).onChange((value) => {
+    noise.set_octaves2(value);
+    draw();
+  });
+
   gui.add(params, "lacunarity", 0, 10).onChange((value) => {
     noise.set_lacunarity(value);
     draw();
@@ -46,9 +95,12 @@ init().then(() => {
     draw();
   });
 
-  noise.set_octaves(params.octaves);
-  noise.set_frequency(params.frequency);
-  gui.add(params, "frequency", 0.005, 0.05).onChange((value) => {
+  gui.add(params, "resolution", 1, 1000).onChange((value) => {
+    params.resolution = Math.floor(value);
+    draw();
+  });
+
+  gui.add(params, "frequency", 1, 1000).onChange((value) => {
     noise.set_frequency(value);
     draw();
   });
@@ -56,23 +108,53 @@ init().then(() => {
   draw();
 
   function draw() {
-    for (let x = 0; x < SIZE; x++) {
-      for (let y = 0; y < SIZE; y++) {
-        const val = (noise.get(x, y) + 1) / 2;
-        const color = step(val, params.threshould) * 255;
-        const index = (y * SIZE + x) * 4;
-        data[index] = color;
-        data[index + 1] = color;
-        data[index + 2] = color;
+    const data = new Uint8ClampedArray(
+      params.resolution * params.resolution * 4
+    );
+    canvas.width = params.resolution;
+    canvas.height = params.resolution;
+    for (let x = 0; x < params.resolution; x++) {
+      for (let y = 0; y < params.resolution; y++) {
+        const val =
+          (noise.get(
+            x / params.resolution / 50,
+            y / params.resolution / 50,
+            params.weight1
+          ) +
+            1) /
+          2;
+        const [r, g, b] = mapTerrain(val + params.threshould);
+        const index = (y * params.resolution + x) * 4;
+        data[index] = r;
+        data[index + 1] = g;
+        data[index + 2] = b;
         data[index + 3] = 255;
       }
     }
-    ctx.putImageData(new ImageData(data, SIZE, SIZE), 0, 0);
+    ctx.putImageData(
+      new ImageData(data, params.resolution, params.resolution),
+      0,
+      0
+    );
   }
 });
 
+function hexToRgb(hex: string): [number, number, number] {
+  return hex
+    .replace("#", "")
+    .match(/.{1,2}/g)!
+    .map((x) => parseInt(x, 16)) as any;
+}
+
 function step(t: number, threshold = 0.5) {
   return t < threshold ? 0 : 1;
+}
+
+function mapTerrain(t: number) {
+  if (t > 0.9) return params.rgbMountain;
+  if (t > 0.72) return params.rgbField;
+  if (t > 0.7) return params.rgbSand;
+  return params.rgbSea;
 }
 
 function sigmoid(t: number, threshold = 0.5) {
--- game_state/src/game_noise.rs
+++ game_state/src/game_noise.rs
@@ -1,50 +1,62 @@
-use noise::{Fbm, MultiFractal, NoiseFn, Seedable, Simplex};
+use noise::{Add, Fbm, MultiFractal, NoiseFn, Perlin, Seedable, Simplex};
 use wasm_bindgen::prelude::*;
 
 type NoiseKind = Simplex;
 
 #[wasm_bindgen]
 pub struct GameNoise {
-    fbm: Fbm<NoiseKind>,
+    fbm1: Fbm<NoiseKind>,
+    fbm2: Fbm<NoiseKind>,
 }
 
 #[wasm_bindgen]
 impl GameNoise {
     pub fn new(seed: Option<u32>) -> Self {
+        let fbm1 = Fbm::<NoiseKind>::new(seed.unwrap_or(0));
+        let fbm2 = Fbm::<NoiseKind>::new(seed.unwrap_or(0));
         Self {
-            fbm: Fbm::<NoiseKind>::new(seed.unwrap_or(0)),
+            fbm1: fbm1.clone(),
+            fbm2: fbm2.clone(),
         }
     }
 
+    pub fn set_frequency2(&mut self, frequency: f64) {
+        self.fbm2 = self.fbm2.clone().set_frequency(frequency);
+    }
+
+    pub fn set_octaves2(&mut self, octaves: usize) {
+        self.fbm2 = self.fbm2.clone().set_octaves(octaves);
+    }
+
     pub fn set_persistence(&mut self, persistence: f64) {
-        self.fbm = self.fbm.clone().set_persistence(persistence);
+        self.fbm1 = self.fbm1.clone().set_persistence(persistence);
     }
 
     pub fn set_frequency(&mut self, frequency: f64) {
-        self.fbm = self.fbm.clone().set_frequency(frequency);
+        self.fbm1 = self.fbm1.clone().set_frequency(frequency);
     }
 
     pub fn set_seed(&mut self, seed: u32) {
-        self.fbm = self.fbm.clone().set_seed(seed);
+        self.fbm1 = self.fbm1.clone().set_seed(seed);
     }
 
     pub fn set_octaves(&mut self, octaves: usize) {
-        self.fbm = self.fbm.clone().set_octaves(octaves);
+        self.fbm1 = self.fbm1.clone().set_octaves(octaves);
     }
 
     pub fn set_lacunarity(&mut self, lacunarity: f64) {
-        self.fbm = self.fbm.clone().set_lacunarity(lacunarity);
+        self.fbm1 = self.fbm1.clone().set_lacunarity(lacunarity);
     }
 
-    pub fn get(&self, x: f64, y: f64) -> f64 {
-        self.fbm.get([x, y])
+    pub fn get(&self, x: f64, y: f64, weight1: f64) -> f64 {
+        self.fbm1.get([x, y]) * weight1 + self.fbm2.get([x, y]) * (1.0 - weight1)
     }
 
     pub fn generate(&self, size: usize) -> Vec<u8> {
         let v: Vec<u8> = (0..size)
             .flat_map(|row| {
                 (0..size).flat_map(move |col| {
-                    let value = self.fbm.get([col as f64, row as f64]);
+                    let value = self.fbm1.get([col as f64, row as f64]);
                     let value = self.step(value);
                     let value = (value + 1.0) * 255.0 / 2.0;
                     let color = (value) as u8;
