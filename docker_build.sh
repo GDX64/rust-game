267eae8a7d27921b3e521be4b08557cab364b006
glmachado <glmachado@nelogica.com.br>
2024-09-14 19:00:22 -0300

    docker build

--- docker_build.sh
+++ docker_build.sh
@@ -1,16 +1,34 @@
 imageName="br_server"
+userName="gdx64"
+tagName=$userName/$imageName
 
 function buildDockerImage(){
+    echo "Building docker image"
     sudo docker build --platform linux/amd64 -t $imageName .
     sudo docker save -o img.tar $imageName
 }
 
 function runImage(){
+    echo "Running docker image"
     ## stop all docker containers running
     sudo docker stop $(docker ps -a -q)
-    sudo docker load -i ./img.tar
+    sudo docker pull $tagName
     sudo docker run -d -p 5000:5000 $imageName
 }
 
-buildDockerImage
-runImage
+function uploadImage(){
+    echo "Uploading docker image"
+    sudo docker tag $imageName $tagName
+}
+
+if [ "$1" == "run" ]; then
+    runImage
+    exit 0
+elif [ "$1" == "build" ]; then
+    buildDockerImage
+    uploadImage
+    exit 0
+elif [ "$1" == "upload" ]; then
+    uploadImage
+    exit 0
+fi
