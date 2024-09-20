7ce21c1e736a04c51c7be7d5516ccbe4f039707c
glmachado <glmachado@nelogica.com.br>
2024-09-19 22:48:35 -0300

    less linewidth

--- game_state/js/Terrain.ts
+++ game_state/js/Terrain.ts
@@ -227,7 +227,7 @@ class MiniMap {
       const height = maxY - minY;
 
       ctx.closePath();
-      ctx.lineWidth = 4;
+      ctx.lineWidth = 2;
       ctx.strokeStyle = "#000000";
       ctx.stroke();
 
