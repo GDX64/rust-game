17a4c94f42cef9dfd8a46e3ea04a1dc77a2dc505
glmachado <glmachado@nelogica.com.br>
2024-10-12 21:33:49 -0300

    reload after route change

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from "vue";
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter, RouteRecordRaw } from "vue-router";
 import App from "./App.vue";
 import Home from "./pages/Home.vue";
 import "./index.css";
 
-const routes = [
+const routes: RouteRecordRaw[] = [
   { path: "/", component: Home },
   {
     path: "/game",
@@ -15,6 +15,11 @@ const routes = [
 const router = createRouter({
   history: createWebHistory("/static/"),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  console.log(to, from);
+  next();
 });
 
 // router.afterEach((to, from, next) => {
--- front/src/pages/Game.vue
+++ front/src/pages/Game.vue
@@ -3,13 +3,21 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 import { ArchpelagusGame } from "../lib";
+import { onBeforeRouteLeave, useRouter } from "vue-router";
 
 const container = ref<HTMLElement>();
+const router = useRouter();
+onBeforeRouteLeave((guard) => {
+  queueMicrotask(() => router.go(0));
+});
 
+let game: ArchpelagusGame | null = null;
 onMounted(async () => {
-  const game = await ArchpelagusGame.new(container.value!);
-  console.log(game);
+  game = await ArchpelagusGame.new(container.value!);
+});
+onUnmounted(() => {
+  game?.destroy();
 });
 </script>
