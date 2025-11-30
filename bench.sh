3f40133c48df12e322f8099a7fd1a72095a0e144
Gabriel Machado <gabriel.delmachado@gmail.com>
2025-11-30 16:03:19 -0300

    visualization

--- bench.sh
+++ bench.sh
@@ -1,11 +1,11 @@
-EXPERIMENT_ID=1 BOT_COUNT=2 INITIAL_SERVER_COUNT=1 docker compose down -v 
-docker compose -f docker-compose-analyze.yml down -v
+EXPERIMENT_ID=1 BOT_COUNT=2 INITIAL_SERVER_COUNT=1 docker compose down  
+docker compose -f docker-compose-analyze.yml down 
 
 experiment=1
-for servers in 1 2 4 6; do
+for servers in 1 2 4 6 8 10; do
   for bots in 2 4 6 8 10; do
       echo "start experiment $experiment with $bots bots and $servers servers..."
-      EXPERIMENT_ID=$experiment BOT_COUNT=$bots INITIAL_SERVER_COUNT=$servers timeout 30 docker compose up --build --abort-on-container-exit
       experiment=$((experiment + 1))
+      EXPERIMENT_ID=$experiment BOT_COUNT=$bots INITIAL_SERVER_COUNT=$servers timeout 30 docker compose up --build 
   done
 done
